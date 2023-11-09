23455df11553c06a328fe72dea3453b863313c93
ddeonseo <eunseo1370@gmail.com>
2023-11-09 15:36:46 +0900

    Initial Commit

---
+++ SeniorHealth/SeniorHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorHealthApp.swift
+//  SeniorHealth
+//
+//  Created by 서은서 on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SeniorHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
